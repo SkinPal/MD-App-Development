b59610eb9bab5329a4f75eeda29057444baf4933
nadzira <nadziraka19@gmail.com>
2024-11-18 15:34:57 +0700

    add calendar to reminder fragment

--- app/src/main/java/com/capstone/skinpal/ui/reminder/ReminderFragment.kt
+++ app/src/main/java/com/capstone/skinpal/ui/reminder/ReminderFragment.kt
@@ -4,25 +4,47 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.capstone.skinpal.R
-import kotlin.getValue
 
-class ReminderFragment : Fragment() {
+class ReminderFragment : Fragment(), HorizontalCalendarAdapter.OnItemClickListener {
 
+    private lateinit var recyclerView: RecyclerView
+    private lateinit var tvDateMonth: TextView
+    private lateinit var ivCalendarNext: ImageView
+    private lateinit var ivCalendarPrevious: ImageView
 
-    private val viewModel: ReminderViewModel by viewModels()
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-        // TODO: Use the ViewModel
-    }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        return inflater.inflate(R.layout.fragment_reminder, container, false)
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_reminder, container, false)
+
+        tvDateMonth = view.findViewById(R.id.text_date_month)
+        ivCalendarNext = view.findViewById(R.id.iv_calendar_next)
+        ivCalendarPrevious = view.findViewById(R.id.iv_calendar_previous)
+        recyclerView = view.findViewById(R.id.recyclerView)
+
+        recyclerView.layoutManager = LinearLayoutManager(
+            requireContext(), LinearLayoutManager.HORIZONTAL, false
+        )
+
+        val calendarSetUp = HorizontalCalendarSetUp()
+        val tvMonth = calendarSetUp.setUpCalendarAdapter(recyclerView, this@ReminderFragment)
+        tvDateMonth.text = tvMonth
+
+        calendarSetUp.setUpCalendarPrevNextClickListener(ivCalendarNext, ivCalendarPrevious, this@ReminderFragment) {
+            tvDateMonth.text = it
+        }
+
+        return view
+    }
+
+    override fun onItemClick(ddMmYy: String, dd: String, day: String) {
+        view?.findViewById<TextView>(R.id.selectedDate)?.text = ddMmYy
     }
 }
