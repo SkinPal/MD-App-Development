7a8002ae9aa40d46a9ad2b3ee1c80c22bbb1376b
nadzira <nadziraka19@gmail.com>
2024-12-12 23:31:15 +0700

    update fragment result

--- app/src/main/java/com/capstone/skinpal/data/local/entity/AnalysisEntity.kt
+++ app/src/main/java/com/capstone/skinpal/data/local/entity/AnalysisEntity.kt
@@ -1,8 +1,6 @@
 package com.capstone.skinpal.data.local.entity
 
-import androidx.room.ColumnInfo
 import androidx.room.Entity
-import androidx.room.PrimaryKey
 import com.capstone.skinpal.data.remote.response.FacialWashItem
 import com.capstone.skinpal.data.remote.response.MoisturizerItem
 import com.capstone.skinpal.data.remote.response.SerumItem
@@ -25,24 +23,12 @@ data class AnalysisEntity(
     val recommendations: String,
     val imageUri: String,
     val publicUrl: String,
-    // Store as JSON string
     val timestamp: Long = System.currentTimeMillis(),
-    @field:SerializedName("moisturizer")
     val moisturizer: List<MoisturizerItem>,
-
-    @field:SerializedName("treatment")
     val treatment: List<TreatmentItem>,
-
-    @field:SerializedName("sunscreen")
     val sunscreen: List<SunscreenItem>,
-
-    @field:SerializedName("toner")
     val toner: List<TonerItem>,
-
-    @field:SerializedName("serum")
     val serum: List<SerumItem>,
-
-    @field:SerializedName("facial_wash")
     val facialWash: List<FacialWashItem>,
 
 )
